ceb9266c05191ca4bbbbf044035d1135ba200446
jon <jonprosser84@gmail.com>
2022-01-01 22:39:55 +0000

    feedback-app-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header.jsx'
+import FeedbackList from './components/FeedbackList.jsx'
+import feedbackData from './data/feedbackData.js'
+import FeedbackStats from './components/FeedbackStats'
 
-function App() {
+const App = () => {
+
+  const [feedback, setFeedback] = useState(feedbackData)
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback}
+          handleDelete={deleteFeedback} />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
